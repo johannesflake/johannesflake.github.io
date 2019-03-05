ea661cef6aa82d6bae2153bf4925cd1478f541ae
johannesflake <johannes.flake@gmail.com>
2019-03-05 08:42:24 +0100

    add bubbles; fine-tune nightsky; update content

--- bubbles.js
+++ bubbles.js
@@ -13,7 +13,6 @@ window.onload = function () {
   canvas.style.left = '0';
   document.body.style.padding = '0';
   document.body.style.margin = '0';
-  document.body.style.backgroundColor = '#eee';
   document.body.appendChild(canvas);
   
   window.onresize = redraw;
@@ -21,7 +20,7 @@ window.onload = function () {
   window.requestAnimationFrame(pt);
 };
 
-var ct = 0, ctGoal = 0;
+var ct = 0;
 function redraw() {
   w = Math.max(w, window.innerWidth);
   h = Math.max(h, window.innerHeight);
@@ -29,34 +28,38 @@ function redraw() {
   canvas.width  = w;
   canvas.height = h;
 
-  var ctx = canvas.getContext('2d');
+  var ctx = canvas.getContext("2d");
   var gradient = ctx.createLinearGradient(0, 0, 0, h);
-  gradient.addColorStop(0, '#eee');
-  gradient.addColorStop(0.7, '#ddd');
-  gradient.addColorStop(1, '#888');
+  gradient.addColorStop(0, '#8df');
+  gradient.addColorStop(0.7, '#28d');
+  gradient.addColorStop(1, '#048');
   ctx.fillStyle = gradient;
   ctx.fillRect(0, 0, w, h);
   
   ct = 0;
-  ctGoal = w*h*0.001;
 }
 
 function pt()  {
-    if (ct < ctGoal) {
+    if (ct < w*h*0.001) {
       ++ct;
       
       var ctx = canvas.getContext('2d'),
         x = Math.random() * w,
         y = Math.random() * h,
-        r = 0.03 * em + Math.pow(Math.random(), 5) * 0.1 * em,
-        b = 0.02 * em + 0.5 * Math.random() * r;
+        r = 0.5 + Math.pow(Math.random(), 5) * 0.5 * em,
+        b = 0.5 + 0.25 * Math.random() * r;
       ctx.beginPath();
-      ctx.fillStyle = '#444';
       ctx.filter = 'blur(' + b + 'px)';
       ctx.arc(x, y, r, 0, 2*Math.PI);
+      var gradient = ctx.createRadialGradient(x, y, 0, x, y, r);
+      gradient.addColorStop(0, "rgba(255,255,255,0.2)");
+      gradient.addColorStop(0.9, "rgba(255,255,255,0.1)");
+      gradient.addColorStop(1, "rgba(255,255,255,0.7)");
+      ctx.fillStyle = gradient;
       ctx.fill();
-      
-      canvas.style.opacity = ct/ctGoal;
+//      ctx.lineWidth = 0.05*em;
+//      ctx.strokeStyle = "rgba(255,255,255,0.8)";
+//      ctx.stroke();
   }
   window.requestAnimationFrame(pt);
 }

ff58ffaa571e89c54014b9c2b188d38b97a5fb91
johannesflake <johannes.flake@gmail.com>
2019-03-13 09:42:51 +0100

    tsp25

--- tsp25/tsp25.js
+++ tsp25/tsp25.js
@@ -1,20 +1,28 @@
+const density = 0.002;
+const time_step_ms = 100;
+const min_dist_em = 0.5;
+
+
 var canvas, w = 0, h = 0, em, div, rect, t0;
 
 window.onload = function () {
   div = document.createElement('div');
   canvas = document.createElement('canvas');
-  if (!canvas.getContext) return;
   em = parseFloat(getComputedStyle(document.body).fontSize);
 
   canvas.width = 0;
   canvas.height = 0;
   canvas.style.cssText = 'position: fixed; z-index:-1; top: 0; left: 0;';
-  div.style.cssText = 'border-radius: 2rem; padding: 1.5rem; height: 1rem; width: 10rem; background-color: #fffe; font-size:1.2rem; display: inline-block; position:absolute;top:30vh;left:30vw; box-shadow: 0 0 1rem black; font-family: monospace;';
+  div.style.cssText = 'border-radius: 2rem; padding: 1.5rem; height: 1rem; width: 10rem; background-color: #fffe; font-size:1.2rem; display: inline-block; position:absolute;top:30vh;left:30vw; box-shadow: 0 0 0.5rem black; font-family: monospace;';
   document.body.style.cssText= 'padding: 0; margin: 0; background: white;';
   document.body.appendChild(canvas);
   document.body.appendChild(div);
   
-  rect = div.getBoundingClientRect();
+  if (!canvas.getContext) {
+    div.style.fontSize = '0.8rem';
+    div.innerHTML = 'Browser not supported.';
+    return;
+  }
   
   window.onresize = redraw;
   redraw();
@@ -24,57 +32,87 @@ window.onload = function () {
 var pts, dist, next, prev;
 function connect(i,j) { next[i] = j; prev[j] = i; }
 function redraw() {
-  w = Math.max(w, window.innerWidth);
-  h = Math.max(h, window.innerHeight);
-  if (w == canvas.width && h == canvas.height)  return;
+  // width and height
+  w = window.innerWidth;
+  h = window.innerHeight;
   canvas.width  = w;
   canvas.height = h;
+  rect = div.getBoundingClientRect();
 
-  // generate points
-  const npts = Math.round(w*h*0.003);
+  // setup...
   const margin = em;
-  pts = []; dist = new Array(npts);
-  for (var i = 0; i<npts; ++i)  dist[i] = new Array(npts);
+  var i, j;
   
+  // generate pts
+  const npts = Math.round((w-2*margin)*(h-2*margin)*density);
+  pts = [];
+  
+  const min_dist = min_dist_em*em;
+  dist = new Array(npts);
+  for (i = 0; i<npts; ++i)  dist[i] = new Array(npts);
   var lst = [];
-  for (var i = 0; i<npts; ++i)  {
-    var pt;
-    do { pt = [Math.random() * w, Math.random()*h];
-    } while (!(pt[0] < rect.left-margin || pt[1] < rect.top-margin || pt[0] > rect.right+margin || pt[1] > rect.bottom+margin)
-      || pt[0] < margin || pt[1] < margin || pt[0] > w-margin || pt[1] > h-margin);
+  
+  for (i = 0; i<npts; ++i)  {
+    const pt = [margin + Math.random() * (w-2*margin),
+                margin + Math.random() * (h-2*margin)];
+    
+    if ((pt[0] > rect.left-margin && pt[1] > rect.top-margin
+      && pt[0] < rect.right+margin && pt[1] < rect.bottom+margin)
+      )  continue;
+
+    const mpts = pts.length;
+    for (j = 0; j<mpts; ++j) {
+      const d = Math.hypot(pt[0]-pts[j][0], pt[1]-pts[j][1]);
+      if (d < min_dist) break;
+      dist[j][mpts] = d;
+    }
+    if (j<mpts) continue;
+    
     pts.push(pt);
-    dist[i][i] = Math.hypot(w,h);
+    lst.push(mpts);
+  }
+  
+  // complete distance array
+  for (i=0; i<pts.length; ++i)
+    for(j=i+1; j<pts.length; ++j)
+      dist[j][i] = dist[i][j];
+  
+  // nearest-neighbor heuristic
+  next = []; prev = [];
+  var lastPt = lst[0];
+  for (i = 0; i<lst.length-1; ++i)  {
+    var dmin, imin;
+    for (j = i+1; j<lst.length; ++j)  {
+      const d = dist[lst[i]][lst[j]];
+      if (j == i+1 ||  d < dmin) {
+        dmin = d;
+        imin = j;
+      }
+    }
+
+    const swp = lst[i+1];
+    lst[i+1] = lst[imin];
+    lst[imin] = swp;
+    
+    connect(lastPt, lst[i+1]);
+    lastPt = lst[i+1];
+  }
+  connect(lastPt, lst[0]);
+  
+  // ini for and start of heuristics
+  iCurrent = lst[0];
+  t0 = new Date().getTime() / 1000;
+  update();
+}
+
+function computeDists() {
+ for (var i = 0; i<pts.length; ++i)  {
     for (var j = 0; j<i; ++j) {
       const d = Math.hypot(pts[i][0]-pts[j][0],pts[i][1]-pts[j][1]);
       dist[i][j] = d;
       dist[j][i] = d;
     }
-    lst.push(i);
   }
-
-  // nearest-neighbor heuristic
-  next = []; prev = [];
-  for (var i = 0; i<npts-1; ++i)  {
-    var dmin, imin;
-    for (var j = i+1; j<npts; ++j)  {
-      const d = dist[lst[i]][lst[j]];
-      if (j == i+1 || d < dmin) {
-        dmin = d;
-        imin = j;
-      }
-    }
-    const swp = lst[i+1];
-    lst[i+1] = lst[imin];
-    lst[imin] = swp;
-    
-    connect(lst[i], lst[i+1]);
-  }
-  connect(lst[npts-1], lst[0]);
-  
-  // setup
-  iCurrent = 0;
-  t0 = new Date().getTime() / 1000;
-  update();
 }
 
 var iCurrent;
@@ -86,15 +124,15 @@ function do2opt(i,j)  {
     from = to; to = ne; ne = prev[to];
     connect(from, to);
   }
-  connect(ni0, nj0)
+  connect(ni0, nj0);
 }
 function fixOne25opt()  {
   var ii = iCurrent, jj;
   for (var i = 0; i < pts.length; ++i, ii=next[ii])  {
     const ni = next[ii];
-    jj = next[ni];
+    jj = ni;
     
-    for (var j = 2; j < pts.length; ++j, jj=next[jj])  {
+    for (var j = 1; j < pts.length; ++j, jj=next[jj])  {
       const nj = next[jj];
       
       // opt-2
@@ -114,7 +152,7 @@ function fixOne25opt()  {
         iCurrent = ii;
         return true;
       }
-      
+
       const pj = prev[jj];
       if (dist[ii][ni]  + dist[jj][nj]  + dist[pj][jj]
         > dist[ii][jj]  + dist[jj][ni]  + dist[pj][nj]  ) {
@@ -146,27 +184,28 @@ function update() {
   ctx.lineWidth = 0.1*em;
   ctx.lineCap = 'round';
   ctx.lineJoin = 'round';
+  ctx.strokeStyle = '#048';
 
   ctx.clearRect(0, 0, w, h);
   
+  // lines
   ctx.beginPath();
-  ctx.strokeStyle = '#048';
-
   var ipt = 0;
   ctx.moveTo(pts[ipt][0], pts[ipt][1]);
-  for (var i=0; i<pts.length; ++i)  {
+  for (var i=0; i < pts.length; ++i)  {
     ipt = next[ipt];
     ctx.lineTo(pts[ipt][0], pts[ipt][1]);
   }
   ctx.stroke();
   
   const dt = Math.round(new Date().getTime()/1000 - t0);
-  const keep = fixOne25opt();
-  div.innerHTML = dt + (keep ? '...' :  ', done!');
+  const keepGoing = fixOne25opt();
+  div.innerHTML = dt + (keepGoing ? '...' :  ', done!');
   
   //noise();
 
   /*
+  // points
   ctx.strokeStyle = 'orange';
   for (const pt of pts) {
     ctx.beginPath();
@@ -175,5 +214,5 @@ function update() {
   }
   */
   
-  if (keep)  window.setTimeout(update, 100);
+  if (keepGoing)  window.setTimeout(update, time_step_ms);
 }

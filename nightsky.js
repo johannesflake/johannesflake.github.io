fc27d00fcdcbdf59188318162dfefa5cd541fa97
johannesflake <flake@art.rwth-aachen.de>
2021-08-27 12:20:09 +0200

    refactor

--- nightsky.js
+++ nightsky.js
@@ -1,17 +1,19 @@
-var canvas, w = 0, h = 0, em;
+var canvas, ctx, w = 0, h = 0, em;
 
 window.onload = function () {
   canvas = document.createElement('canvas');
+  canvas.style.cssText = 'position: fixed; z-index: -1; top: 0; left: 0;';
+  
   if (!canvas.getContext) return;
+  ctx = canvas.getContext('2d');
+  ctx.fillStyle = '#555';
+
   em = parseFloat(getComputedStyle(document.body).fontSize);
 
-  canvas.width = 0;
-  canvas.height = 0;
-  canvas.style.cssText = 'position: fixed; z-index: -1; top: 0; left: 0;';
-  document.body.appendChild(canvas);
-  
   window.onresize = redraw;
+
   redraw();
+  document.body.appendChild(canvas);
   window.requestAnimationFrame(pt);
 };
 
@@ -26,7 +28,7 @@ function redraw() {
   canvas.width  = w;
   canvas.height = h;
 
-  canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   
   ct = 0;
   ctGoal = w*h*0.001;
@@ -40,8 +42,6 @@ function pt()  {
     const r = 0.03 * em + Math.pow(Math.random(), 5) * 0.1 * em,
       b = 0.04 * em + 0.5 * Math.random() * r;
   
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = '#555';
     ctx.filter = 'blur(' + b + 'px)';
       
     for (var i = 0; i < npts; ++i)  {
@@ -50,6 +50,7 @@ function pt()  {
       ctx.arc(x, y, r, 0, 2*Math.PI);
       ctx.fill();
     }
+  
+    window.requestAnimationFrame(pt);
   }
-  window.requestAnimationFrame(pt);
 }

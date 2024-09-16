6ecf04ae087a17a1528cf42aceed6be2e9ce1692
johannesflake <flake@art.rwth-aachen.de>
2024-09-16 20:20:27 +0200

    v0 of rep24

--- rep24/diags.js
+++ rep24/diags.js
@@ -12,9 +12,6 @@ function pickN(n) {
 function pick(arr)  {
   return arr[pickN(arr.length)];
 }
-function perturbation(amp)  {
-  return (2*Math.random()-1)*amp;
-}
 function randomArcs(k) {
   var arr = [];
   var randomInsert = (arr, it) => arr.splice(pickN(arr.length), 0, it);
@@ -33,17 +30,84 @@ function makePath(d, cl="") {
   if (cl!=="") el.setAttribute("class", cl);
   return el;
 }
-function makeDiag(rows, cols, bg = "white", fg = "#eee") {
-  var ux = 10, uy = 12,
-    axpert = 0.2, aypert = 0.1,
-    wline = 0.85, wouterline = 4*wline;
 
+
+var dunit = 8;
+var ux = 10, uy = 12,
+  axpert = 0.2, aypert = 0.1,
+  wline = 0.85, wouterline = 4*wline;
+
+function makeGrid(w,h)  {
+  var qvacuum = 0.02, qgrowx = 0.25, qgrowy = 0.25;
+  var nsteps = 20;
+  
+  var arr = [...new Array(w)].map(() => new Array(h).fill(false));
+  var box = (x,y) => arr[(x+w)%w][(y+h)%h];
+  for (var i=0; i<nsteps; ++i) for (var x=0; x<w; ++x) for (var y=0; y<h; ++y) {
+    if (box(x,y)) continue;
+    var r=Math.random();
+    
+    if (
+      (box(x-1,y) && !box(x+1,y) && !box(x+1,y-1) && !box(x+1,y+1) && (box(x,y-1) || !box(x-1,y-1)) && r<qgrowx) ||
+      (box(x+1,y) && !box(x-1,y) && !box(x-1,y-1) && !box(x-1,y+1) && (box(x,y+1) || !box(x+1,y+1)) && r<qgrowx) ||
+      (box(x,y-1) && !box(x,y+1) && !box(x-1,y+1) && !box(x+1,y+1) && (box(x-1,y) || !box(x-1,y-1)) && r<qgrowy) ||
+      (box(x,y+1) && !box(x,y-1) && !box(x-1,y-1) && !box(x+1,y-1) && (box(x+1,y) || !box(x+1,y+1)) && r<qgrowy) ||
+      (!box(x-1,y) && !box(x+1,y) && !box(x,y-1) && !box(x,y+1) && !box(x-1,y-1) && !box(x+1,y+1) && !box(x+1,y-1) && !box(x-1,y+1) && r<qvacuum) )
+        arr[x][y] = true;
+  }
+  // ////////////////////// ///////////////////
+  function perturbation(amp)  {
+    return (2*Math.random()-1)*amp;
+  }
+  var pert = Array(w).fill().map(
+    () => Array(h).fill().map(() => [perturbation(axpert),perturbation(aypert)] ) );
+
+  // /////////////// //////////////
+  var lines = [], toggle=false;
+  for (var x=0; x<=w; ++x) { for (var y=0; y<h; ++y)  {
+      if ((box(x,y) || box(x-1,y)) == toggle) continue;
+      lines.push([x,y]);
+      toggle = !toggle;
+    }
+    if (toggle) {
+      lines.push([x,h]);
+      toggle = !toggle;
+    }
+  }
+  for (var y=0; y<=h; ++y) { for (var x=0; x<=w; ++x)  {
+      if ((box(x,y) || box(x,y-1)) == toggle) continue;
+      lines.push([x,y]);
+      toggle = !toggle;
+    }
+    if (toggle) {
+      lines.push([w,y]);
+      toggle = !toggle;
+    }
+  }
+  
+  // /////////// //////////////////
+  var result = createSvgEl("g");
+  for (var i=0; i<lines.length; i+=2) {
+    var x = lines[i][0]*dunit, y=lines[i][1]*dunit,
+      dx = (lines[i+1][0]-lines[i][0])*dunit,
+      dy=(lines[i+1][1]-lines[i][1])*dunit;
+    var path = `M ${x} ${y} c ${0} ${0}, ${0} ${0}, ${dx} ${dy}`;
+    result.append(makePath(path));
+  }
+  return result;
+}
+
+function makeDiag(rows, cols, bg = "white", fg = "#eee") {
   var svg = createSvgEl("svg");
-  svg.setAttribute("viewBox", `0 0 ${cols*ux} ${rows*uy}`);
+  svg.setAttribute("viewBox", `0 0 ${cols*dunit} ${rows*dunit}`);
 
   var style = createSvgEl("style");
   style.innerHTML = `path {fill: transparent; stroke-linecap:butt; stroke:${fg}; stroke-width:${wline};} path.outer {stroke:${bg}; stroke-width:${wouterline};}`;
   svg.append(style);
+
+  svg.append(makeGrid(rows,cols));
+  return svg;
+  
 
   var pert = Array(rows).fill().map(
     () => Array(cols).fill().map(() => perturbation(axpert) ) );
